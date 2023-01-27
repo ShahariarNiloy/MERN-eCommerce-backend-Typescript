89a77d8b265803dfb912ddd433d1aa25b58aacdc
ShahariarNiloy <shahariarniloyblv@gmail.com>
2023-01-28 01:11:37 +0600

    products edit

--- controllers/productController.ts
+++ controllers/productController.ts
@@ -6,18 +6,94 @@ export const createProduct = async (
   res: Response,
   next: NextFunction
 ) => {
-  const product = await Product.create(req.body);
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json({
+      success: true,
+      product,
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      res.status(500).json({ success: false, message: err.message });
+    } else {
+      res.status(500).json({ success: false, message: "Something went wrong" });
+    }
+  }
+};
 
-  res.status(201).json({
+export const getAllProducts = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json({ success: true, products });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      res.status(500).json({ success: false, message: err.message });
+    } else {
+      res.status(500).json({ success: false, message: "Something went wrong" });
+    }
+  }
+};
+
+export const updateProduct = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    //   return next(new ErrorHandler("Product not found", 404));
+    return res
+      .status(500)
+      .json({ success: false, message: "Product not found" });
+  }
+
+  // Images Start Here
+  let images = [];
+
+  if (typeof req.body.images === "string") {
+    images.push(req.body.images);
+  } else {
+    images = req.body.images;
+  }
+
+  if (images !== undefined) {
+    // req.body.images = imagesLinks;
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
     success: true,
     product,
   });
 };
 
-export const getAllProducts = (
+export const deleteProduct = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  res.status(200).json({ message: "Route is working fine !!" });
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Product not found" });
+  }
+
+  await product.remove();
+
+  res.status(200).json({
+    success: true,
+    message: "Product Delete Successfully",
+  });
 };
--- routes/productRoute.ts
+++ routes/productRoute.ts
@@ -2,11 +2,15 @@ import express from "express";
 import {
   createProduct,
   getAllProducts,
+  updateProduct,
 } from "../controllers/productController";
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
+// router.route("/product/:id").get(getProductDetails);
 router.route("/admin/product/new").post(createProduct);
+router.route("/admin/product/:id").put(updateProduct);
+//   .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
 
 export default router;
