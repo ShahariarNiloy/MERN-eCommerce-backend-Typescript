4555623bb0b663c57af18c46c9c505b6bc23bf38
ShahariarNiloy <shahariarniloyblv@gmail.com>
2023-02-07 12:47:24 +0600

    nodemailer mailtrap configuration

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -7,22 +7,27 @@ interface MailOptionType {
 }
 
 const sendEmail = async (options: MailOptionType): Promise<void> => {
-    const transporter = nodeMailer.createTransport({
-        host: process.env.SMPT_HOST as string,
-        auth: {
-            user: process.env.SMPT_MAIL,
-            pass: process.env.SMPT_PASSWORD,
-        },
-    })
+    try {
+        const transporter = nodeMailer.createTransport({
+            host: process.env.SMPT_HOST as string,
+            port: Number(process.env.SMPT_PORT),
+            auth: {
+                user: process.env.SMPT_USER,
+                pass: process.env.SMPT_PASSWORD,
+            },
+        })
 
-    const mailOptions = {
-        from: process.env.SMPT_MAIL,
-        to: options.email,
-        subject: options.subject,
-        text: options.message,
+        const mailOptions = {
+            from: process.env.SMPT_MAIL,
+            to: options.email,
+            subject: options.subject,
+            text: options.message,
+        }
+
+        await transporter.sendMail(mailOptions)
+    } catch (err) {
+        console.log(err)
     }
-
-    await transporter.sendMail(mailOptions)
 }
 
 export default sendEmail
